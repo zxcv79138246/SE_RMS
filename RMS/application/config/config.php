ba33d69629b22565067a298adb54f26f5f66cfcc
劉至峻 <Chih-Chun_Liu@liuzhijundeMacBook-Pro.local>
2015-11-26 19:10:03 +0800

    config-> config 更改網址到RMS confit-> datamase 更改使用者為se_rms 更改資料夾名稱為RMS

--- RMS/application/config/config.php
+++ RMS/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://se-rms.ddns.net/CodeIgniter';
+$config['base_url'] = 'http://se-rms.ddns.net/RMS';
 
 /*
 |--------------------------------------------------------------------------
