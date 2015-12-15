ecf5ab169d9c9c7d29b1164270bc01c459e0f48f
劉至峻 <Chih-Chun_Liu@liuzhijundeMacBook-Pro.local>
2015-12-15 16:39:46 +0800

    更新bootstram版本到3.3.6

--- RMS/application/views/layout/header.php
+++ RMS/application/views/layout/header.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Posts</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	</head>
 	<body>
