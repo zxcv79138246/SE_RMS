26d7bdd3771c02bb71cf142b16ffca6a693810ff
Ye Wei Lun <ulsine83@gmail.com>
2015-12-17 12:03:42 +0800

    Model: R&R , R&T

---
+++ RMS/application/models/R_and_r_relation_model.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class R_and_r_relation_model extends MY_Model{
+
+	public $table = 'R_and_r_relation';
+	protected $primaryKey = array('r_id1', 'r_id2');
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function find($r_id1, $r_id2)
+	{
+		$query = $this->db->getwhere($this->table, 
+			[$primaryKey[0] => $r_id1, $primaryKey[1] => $r_id2]);
+		if($query->result())
+			return $query->result[0];
+		else
+			return false;
+	}
+}
---
+++ RMS/application/models/R_and_t_relation_model.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class R_and_t_relation_model extends MY_Model{
+
+	public $table = 'r_and_t_relation';
+	protected $primaryKey = array('r_id', 't_id');
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function find($r_id, $t_id)
+	{
+		$query = $this->db->getwhere($this->table, 
+			[$primaryKey[0] => $r_id, $primaryKey[1] => $t_id]);
+		if($query->result())
+			return $query->result[0];
+		else
+			return false;
+	}
+}
