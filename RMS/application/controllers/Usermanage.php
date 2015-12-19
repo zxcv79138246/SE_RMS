ce7e50650ed742ed230bce5202c7ba3ef086ba9f
劉至峻 <Chih-Chun_Liu@liuzhijundeMacBook-Pro.local>
2015-12-19 15:31:01 +0800

    修正 usermanage redirect錯誤

--- RMS/application/controllers/Usermanage.php
+++ RMS/application/controllers/Usermanage.php
@@ -14,7 +14,7 @@ class Usermanage extends CI_Controller
 		{
 			$this->session->set_flashdata('message', '權限不足');
 			$this->session->set_flashdata('type', 'danger');
-			redirict('/index');
+			redirect('/index');
 		}
 	}
 
@@ -22,5 +22,15 @@ class Usermanage extends CI_Controller
 	{
 		$users = $this->user->all();
 		$this->twig->display('rms/usermanage/usermanage.html', compact('users'));
+	}
+
+	public function destory($u_id)
+	{
+		if ($destoried = $this->user->destory(['u_id'=>$u_id]))
+		{
+			$this->session->set_flashdata('message',"{$destoried[0]->name} 已被刪除");
+			$this->session->set_flashdata('type','warning');
+		}
+		redirect('/usermanage');
 	}
 }
