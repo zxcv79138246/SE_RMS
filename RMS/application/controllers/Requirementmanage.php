cf0fb281ff1ed7377142bc51562ee846e01a5e3c
WSX741405 <wsx741405@gmail.com>
2015-12-31 00:06:47 +0800

    主選單新增  需求管理

---
+++ RMS/application/controllers/Requirementmanage.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Requirementmanage extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Requirement_model', 'requirement');
+	}
+
+	public function index()
+	{
+		$u_id = $this->session->userdata('u_id');
+		$requirements = $this->requirement->all();
+		$this->twig->display('rms/requirementmanage/requirementmanage.html', compact('requirements'));
+	}
+}
+?>
