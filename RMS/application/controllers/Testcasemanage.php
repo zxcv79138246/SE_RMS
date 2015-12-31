c5ee595b5adddfa31e3fc0c86ac025bcb1dfd468
Ye Wei Lun <ulsine83@gmail.com>
2015-12-31 16:12:39 +0800

    簡易Testcase列表、新增、刪除

---
+++ RMS/application/controllers/Testcasemanage.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Testcasemanage extends CI_Controller
+{
+	private $currentProject = 135;
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('testcase_model','testcase');
+	}
+
+	public function selectProject($p_id)
+	{
+		$this->currentProject=$p_id;
+		$testcases = $this->testcase->where(['p_id'=>$this->currentProject]);
+		$this->twig->display("rms/testcasemanage/testcasemanage.html",compact("testcases"));
+	}
+
+	public function index()
+	{
+		$testcases = $this->testcase->where(['p_id'=>$this->currentProject]);
+		$this->twig->display("rms/testcasemanage/testcasemanage.html",compact("testcases"));
+	}
+
+	public function create()
+	{
+		$this->twig->display("rms/testcasemanage/create.html");	
+	}
+
+	public function store()
+	{
+		$data =
+		[
+			'name' => $this->input->post('testcaseName'),
+			'description' => $this->input->post('testcaseDescription'),
+			'input_data' => $this->input->post('testcaseInput_data'),
+			'expected_results' => $this->input->post('testcaseExpected_results'),
+			'owner'	=> $this->session->userdata('u_id'),
+			'p_id'	=> $this->currentProject,
+			'attachment' => $this->input->post('testcaseAttachment'),
+		];
+		if($this->testcase->duplicateCheck(['name'=>$data['name'],'p_id'=>$data['p_id']],1))
+		 {
+				$this->session->set_flashdata('message', "Testcase名稱重複");
+				$this->session->set_flashdata('type', 'danger');
+
+		}
+		else 
+		{
+				//$this->testcase->insert($data);
+				$this->session->set_flashdata('message', "TestCase {$data['name']} 新增成功");
+				$this->session->set_flashdata('type', 'success');
+		}	
+		redirect('/testcasemanage');
+	}
+
+	public function destroy($t_id)
+	{	
+		if ($destoried = $this->testcase->destory(['t_id'=>$t_id]))
+		{
+			$this->session->set_flashdata('message',"{$destoried[0]->name} 已被刪除");
+			$this->session->set_flashdata('type','warning');
+		}
+		redirect('/testcasemanage');
+	}
+} 
--- RMS/application/models/testcase_model.php
+++ RMS/application/models/testcase_model.php
@@ -24,4 +24,22 @@ class Testcase_model extends MY_Model {
     	else
     		return false;
     }
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Testcase_model extends MY_Model {
+
+	public $table = 'test_case';
+    protected $primaryKey = 't_id';
+
+    public function duplicateCheck($data, $is_create = 0)  //驗證使否有重複內容
+    {
+        $this->db->from($this->table);
+        foreach ($data as $key => $value) {
+            $this->db->where($key);
+        }
+        $query = $this->db->get();
+        return ($query->num_rows() + $is_create) > 1;
+    }
+
+
 }
